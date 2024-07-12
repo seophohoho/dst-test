94d6da5f283a837d0429fa52e45e831def10d237
seophohoho <seop0937@gmail.com>
2024-07-12 16:49:40 +0900

    Fix port #3

--- test.py
+++ test.py
@@ -5,10 +5,6 @@ import time
 
 from constants import *
 
-import socket
-import threading
-import time
-
 def random_hex():
     return '{:02x}'.format(random.randint(0, 255))
 
@@ -19,15 +15,18 @@ def generate_hex(count):
     return str_hex+"FF"
 
 class SetTopBox:
-    def __init__(self, host, port):
+    def __init__(self, host, port, local_port=None):
         self.host = host
         self.port = port
+        self.local_port = local_port
         self.socket = None
         self.connect()
 
     def connect(self):
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if self.local_port:
+                self.socket.bind(('0.0.0.0', self.local_port))  # 로컬 포트를 바인딩
             self.socket.connect((self.host, self.port))
             print(f"Connected to {self.host}:{self.port}")
             threading.Thread(target=self.receive_messages, daemon=True).start()
@@ -35,6 +34,7 @@ class SetTopBox:
         except Exception as e:
             print(f"Failed to connect: {e}")
             time.sleep(5)
+            self.connect()
 
     def send_message(self, message):
         try:
@@ -42,7 +42,7 @@ class SetTopBox:
             print(f"Sent: {message}")
         except Exception as e:
             print(f"Failed to send message: {e}")
-            self.handle_disconnect()
+            self.disconnect()
 
     def receive_messages(self):
         while True:
@@ -65,20 +65,20 @@ class SetTopBox:
 
     def close(self):
         self.socket.close()
-    
+
     def send_status(self):
         while True:
             self.send_message(generate_hex(32))
             time.sleep(10)
-    
+
     def request_time_data(self):
         self.send_message("FE093333333300FF")
-    
+
     def send_ping_addr(self):
         self.send_message(generate_hex(32))
 
 if __name__ == "__main__":
-    client = SetTopBox(HOST, PORT)
+    client = SetTopBox(HOST, SERVER_PORT, LOCAL_PORT)
     try:
         while True:
             time.sleep(1)
